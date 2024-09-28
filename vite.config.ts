d8823b8b398cabdc86964ce3b4630a44759f1a8c
May Marie <lalanganph@gmail.com>
2024-09-28 23:18:14 +0200

    edit: vite.config.ts base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { configDefaults } from 'vitest/config'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movie-streamer/',
+  base: '/Movie-streamer/',
   test: {
     ...configDefaults,
     globals: true,
