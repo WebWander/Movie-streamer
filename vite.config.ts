6d07d5fc8e9c633e410fc6b6250ee87c83458511
May Marie <lalanganph@gmail.com>
2024-09-28 22:03:16 +0200

    add: ci/cd workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/movie-streamer/',
+  test: {
+    ...configDefaults,
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts', // Optional if you have a test setup file
+  },
 })
